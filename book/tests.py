582edddee18aaa87f93b637a882b65938beed9df
Vitaly <47782238+MyCoRIIIiK@users.noreply.github.com>
2020-05-15 11:20:37 +0300

    Update tests.py

--- book/tests.py
+++ book/tests.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 
 import django
 import pytest
@@ -13,12 +14,6 @@ from django.test import TestCase
 from book.apps import BookConfig
 from book.models import Genre,Author,Book
 
-
-# class URLTests(TestCase):
-#
-#     def test_homepage(self):
-#         response = self.client.get('/')
-#         self.assertEqual(response.status_code,200)
 
 @pytest.mark.django_db
 def test_authors():
@@ -74,3 +69,47 @@ def test_authors2():
     book = Book.objects.create(name='book',year='2020',genre=genre,author=authors,describe='describe',picture='smth.jpg')
     book.save()
     assert Book.objects.count() == 1
+
+
+# @pytest.fixture
+# @pytest.mark.django_db
+# def test_auth_view(auto_login_user):
+#     client, user = auto_login_user()
+#     url = reverse('login')
+#     response = client.get(url)
+#     assert response.status_code == 200
+
+
+@pytest.fixture
+def test_password():
+    return 'strong-test-pass'
+
+
+@pytest.fixture()
+def create_user(db, django_user_model, test_password):
+    def make_user(**kwargs):
+        kwargs['password'] = test_password
+        if 'username' not in kwargs:
+            kwargs['username'] = str(uuid.uuid4())
+        return django_user_model.objects.create_user(**kwargs)
+
+    return make_user
+
+
+@pytest.fixture
+def auto_login_user(db, client, create_user, test_password):
+    def make_auto_login(user=None):
+        if user is None:
+            user = create_user()
+        client.login(username=user.username, password=test_password)
+        return client, user
+
+    return make_auto_login
+
+
+@pytest.mark.django_db
+def test_auth_view(auto_login_user):
+    client, user = auto_login_user()
+    url = reverse('login')
+    response = client.get(url)
+    assert response.status_code == 200
