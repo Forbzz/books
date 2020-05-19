59a7ed12d7ee9cff20be7ab2c79477b6c7f8bee4
Vitaly <47782238+MyCoRIIIiK@users.noreply.github.com>
2020-05-19 12:49:03 +0300

    Update settings.py

--- libraryapp/settings.py
+++ libraryapp/settings.py
@@ -214,3 +214,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+import django_heroku
+django_heroku.settings(locals())
